d0d58a569950f0ee3d1db5e5d1f6e600c7a3cef0
PC <dieuhang091095@gmail.com>
2017-06-10 00:22:15 +0700

    fix layout

--- app/src/main/java/demo/MainActivity.java
+++ app/src/main/java/demo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_demo);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycleView);
         List<String> names = new ArrayList<>();
         names.add("Nguyễn Văn A");
