e84f156c3e8c25f2b1563b46ce56b3e5d687645c
thien <root@at-K501LB.com>
2017-06-12 17:17:56 +0700

    add list item

--- app/src/main/java/vn/asiantech/internship/views/DrawerFragment.java
+++ app/src/main/java/vn/asiantech/internship/views/DrawerFragment.java
@@ -2,6 +2,7 @@ package vn.asiantech.internship.views;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,7 +31,7 @@ public class DrawerFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_drawer, container, false);
 
         mRecyclerView = (RecyclerView) v.findViewById(R.id.rvDrawer);
-
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mListDrawer = getResources().getStringArray(R.array.listDrawer);
         mRecyclerView.setAdapter(new DrawerAdapter(mListDrawer));
 
