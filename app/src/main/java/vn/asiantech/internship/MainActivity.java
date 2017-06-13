c948192f04cb6113982c0b5127f00a93b8fc775f
hainq94 <hainq94@gmail.com>
2017-06-13 17:38:42 +0700

    first commit

--- app/src/main/java/vn/asiantech/internship/MainActivity.java
+++ app/src/main/java/vn/asiantech/internship/MainActivity.java
@@ -2,9 +2,10 @@ package vn.asiantech.internship;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
