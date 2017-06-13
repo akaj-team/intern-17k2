69eb6397b2426d4b62b8ef736f6a1a42cc57c314
Cuong Cao <vancuong.itf@gmail.com>
2017-06-13 17:06:20 +0700

    final

---
+++ app/src/main/java/vn/asiantech/internship/models/MyData.java
@@ -0,0 +1,24 @@
+package vn.asiantech.internship.models;
+
+import java.io.Serializable;
+
+import vn.asiantech.internship.MainActivity;
+
+/**
+ * Created by PC on 6/13/2017.
+ */
+public class MyData implements Serializable {
+    private MainActivity.OnClick listener;
+
+    public MyData(MainActivity.OnClick listener) {
+        this.listener = listener;
+    }
+
+    public MainActivity.OnClick getListener() {
+        return listener;
+    }
+
+    public void setListener(MainActivity.OnClick listener) {
+        this.listener = listener;
+    }
+}
