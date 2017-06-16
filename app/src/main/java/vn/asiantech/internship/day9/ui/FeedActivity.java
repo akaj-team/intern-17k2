4acf1c757e87aa22ec48b64695283e24c313aacb
rimoka95 <vothihoabkdn@gmail.com>
2017-06-16 09:13:44 +0700

    fix ci

--- app/src/main/java/vn/asiantech/internship/day9/ui/FeedActivity.java
+++ app/src/main/java/vn/asiantech/internship/day9/ui/FeedActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 
 import vn.asiantech.internship.R;
 
+/**
+ * create FeedActivity
+ * create by at-hoavo on 16/06/2017
+ */
 public class FeedActivity extends AppCompatActivity {
 
     @Override
