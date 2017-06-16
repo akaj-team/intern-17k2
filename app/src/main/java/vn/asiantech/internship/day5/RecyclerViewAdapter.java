b8144f9c518b2df0d6705edcd764baa33ca25bd3
rimoka95 <vothihoabkdn@gmail.com>
2017-06-16 08:47:27 +0700

    finish

--- app/src/main/java/vn/asiantech/internship/day5/RecyclerViewAdapter.java
+++ app/src/main/java/vn/asiantech/internship/day5/RecyclerViewAdapter.java
@@ -1,4 +1,4 @@
-package vn.asiantech.internship.Day5;
+package vn.asiantech.internship.day5;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
---
+++ app/src/main/java/vn/asiantech/internship/day9/ui/FeedFragment.java
@@ -0,0 +1,45 @@
+package vn.asiantech.internship.day9.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.asiantech.internship.R;
+import vn.asiantech.internship.day9.adapter.FeedRecyclerViewAdapter;
+import vn.asiantech.internship.day9.model.User;
+
+/**
+ * Created by rimoka on 15/06/2017.
+ */
+public class FeedFragment extends Fragment {
+    private List<User> mUsers = new ArrayList<>();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_feed, container, false);
+        RecyclerView recyclerViewFeed = (RecyclerView) v.findViewById(R.id.recyclerViewFeed);
+        if (getActivity() instanceof FeedActivity) {
+            String[] users = getActivity().getResources().getStringArray(R.array.list_user);
+            String[] descriptions = getActivity().getResources().getStringArray(R.array.list_description);
+            for (int i = 0; i < users.length; i++) {
+                mUsers.add(new User(users[i], descriptions[i], R.mipmap.ic_avatar));
+            }
+            FeedRecyclerViewAdapter feedAdapter = new FeedRecyclerViewAdapter(getActivity(), mUsers);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+            linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerViewFeed.setLayoutManager(linearLayoutManager);
+            recyclerViewFeed.setAdapter(feedAdapter);
+        }
+
+        return v;
+    }
+}
