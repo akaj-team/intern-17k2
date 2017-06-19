d9c889cf2b18cf9db98976906a0de5134612f8ab
at-ducle <leanhduc2015@gmail.com>
2017-06-19 16:25:43 +0700

    first commit

---
+++ app/src/main/java/vn/asiantech/internship/ui/main/NoteActivity.java
@@ -0,0 +1,25 @@
+package vn.asiantech.internship.ui.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import vn.asiantech.internship.R;
+
+/**
+ * Created by ducle on 19/06/2017.
+ */
+
+public class NoteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 0);
+        setContentView(R.layout.activity_note);
+
+
+    }
+}
