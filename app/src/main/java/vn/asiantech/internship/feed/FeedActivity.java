d6da7b760ef3bd7c02231a5008778212663cc092
hainq94 <hainq94@gmail.com>
2017-06-19 16:34:25 +0700

    add recyclerview

---
+++ app/src/main/java/vn/asiantech/internship/feed/FeedActivity.java
@@ -0,0 +1,19 @@
+package vn.asiantech.internship.feed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.asiantech.internship.R;
+
+/**
+ * Activity contain Feedfragment
+ * <p>
+ * Created by Hai on 6/15/2017.
+ */
+public class FeedActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
