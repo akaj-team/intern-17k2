c2c97de6ff6bf58f5c5b4f83b752527c670fea16
Dinh Vo X <ignore8542@gmail.com>
2017-06-25 19:59:18 +0700

    update UI

--- app/src/main/java/vn/asiantech/internship/drawer/ui/splash/SplashActivity.java
+++ app/src/main/java/vn/asiantech/internship/drawer/ui/splash/SplashActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import vn.asiantech.internship.R;
+import vn.asiantech.internship.drawer.day15.TestActivity;
 import vn.asiantech.internship.drawer.ui.main.MainActivity;
 
 /**
@@ -21,6 +22,13 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.btnBai15).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, TestActivity.class));
             }
         });
     }
