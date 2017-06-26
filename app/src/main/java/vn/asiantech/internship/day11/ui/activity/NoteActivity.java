21686731d667748878a0007b18c5e0438c094b1c
rimoka95 <vothihoabkdn@gmail.com>
2017-06-26 12:50:43 +0700

    fix back stack

--- app/src/main/java/vn/asiantech/internship/day11/ui/activity/NoteActivity.java
+++ app/src/main/java/vn/asiantech/internship/day11/ui/activity/NoteActivity.java
@@ -15,6 +15,7 @@ import vn.asiantech.internship.day11.ui.fragment.NoteFragment;
  * Created by at-hoavo on 19/06/2017.
  */
 public class NoteActivity extends AppCompatActivity {
+    public static final String TYPE_NOTE="Note Fragment";
     private NoteFragment mNoteFragment = new NoteFragment();
     FragmentTransaction mTransaction;
 
@@ -26,11 +27,23 @@ public class NoteActivity extends AppCompatActivity {
     }
 
     public void changeFragment(Fragment fragment) {
-        String tagFragment = fragment.getClass().getName();
+        String tagFragment=fragment.getClass().getName();
+        if(fragment instanceof NoteFragment){
+            tagFragment=TYPE_NOTE;
+        }
         FragmentManager manager = getSupportFragmentManager();
         mTransaction = manager.beginTransaction();
-        mTransaction.replace(R.id.flNote, fragment);
+        mTransaction.replace(R.id.flNote, fragment,tagFragment);
         mTransaction.addToBackStack(tagFragment);
         mTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        if( fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount()-1).equals(fragmentManager.findFragmentByTag(TYPE_NOTE))){
+            finish();
+        }
     }
 }
