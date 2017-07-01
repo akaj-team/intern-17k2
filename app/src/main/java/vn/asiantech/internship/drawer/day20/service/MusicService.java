6b6bcddc431c02f3f162ee01d1a5ee883e6fa21d
Dinh Vo X <ignore8542@gmail.com>
2017-07-01 22:31:13 +0700

    create service

---
+++ app/src/main/java/vn/asiantech/internship/drawer/day20/service/MusicService.java
@@ -0,0 +1,56 @@
+package vn.asiantech.internship.drawer.day20.service;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MusicService extends Service {
+
+    public static final String ACTION_PLAY = "play";
+    public static final String ACTION_PAUSE = "pause";
+    public static final String ACTION_NEXT = "next";
+    public static final String ACTION_PREVIOUS = "previous";
+    public static final String ACTION_SHUFFLE = "shuffle";
+    public static final String ACTION_AUTONEXT = "auto_next";
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()){
+                case ACTION_PLAY:
+
+                    break;
+                case ACTION_PAUSE:
+                    break;
+                case ACTION_NEXT:
+                    break;
+                case ACTION_PREVIOUS:
+                    break;
+                case ACTION_SHUFFLE:
+                    break;
+                case ACTION_AUTONEXT:
+                    break;
+                default:
+
+            }
+        }
+    };
+
+    public MusicService() {
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/vn/asiantech/internship/drawer/day20/ui/MusicFragment.java
@@ -0,0 +1,95 @@
+package vn.asiantech.internship.drawer.day20.ui;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import vn.asiantech.internship.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MusicFragment extends Fragment {
+
+    public static final String CURRENT_TIME = "current_time";
+
+    private ImageButton mImgBtnPlay;
+    private ImageButton mImgBtnPrevious;
+    private ImageButton mImgBtnNext;
+    private CircleImageView mCircleImageViewMusic;
+    private SeekBar mSeekBar;
+    private TextView mTvMusicTime;
+    private TextView mCurrentTime;
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals("CURRENT_TIME")) {
+                // TODO: 30/06/2017
+            }
+        }
+    };
+
+    public MusicFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_music, container, false);
+        initUI(layout);
+        return layout;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        addEvents();
+    }
+
+    private void addEvents() {
+        mImgBtnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mImgBtnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mImgBtnPrevious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void initUI(View layout) {
+        mImgBtnPlay = (ImageButton) layout.findViewById(R.id.imgBtnPlay);
+        mImgBtnNext = (ImageButton) layout.findViewById(R.id.imgBtnNext);
+        mImgBtnPrevious = (ImageButton) layout.findViewById(R.id.imgBtnPrevious);
+        mCircleImageViewMusic = (CircleImageView) layout.findViewById(R.id.imgMusic);
+        mSeekBar = (SeekBar) layout.findViewById(R.id.seekBar);
+        mCurrentTime = (TextView) layout.findViewById(R.id.tvCurrentTime);
+        mTvMusicTime = (TextView) layout.findViewById(R.id.tvTimeSong);
+    }
+}
