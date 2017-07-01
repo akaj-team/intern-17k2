46a7524b023a7e25be16fb24e50e326b8ab16f5e
rimoka95 <vothihoabkdn@gmail.com>
2017-07-01 23:21:25 +0700

    add ui

---
+++ app/src/main/java/vn/asiantech/internship/day19/activity/MusicActivity.java
@@ -0,0 +1,15 @@
+package vn.asiantech.internship.day19.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.asiantech.internship.R;
+
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+}
