60aced6b0ddf6c5e703cbcc23e533df1f15b462b
at-thiennguyen <nguyenthanhthien2031@gmail.com>
2017-07-04 04:24:49 +0700

    fix convention

--- app/src/main/java/vn/asiantech/internship/ui/json/ContactActivity.java
+++ app/src/main/java/vn/asiantech/internship/ui/json/ContactActivity.java
@@ -14,6 +14,6 @@ public class ContactActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_json);
+        setContentView(R.layout.activity_contact);
     }
 }
--- app/src/main/java/vn/asiantech/internship/ui/json/ContactFragment.java
+++ app/src/main/java/vn/asiantech/internship/ui/json/ContactFragment.java
@@ -30,7 +30,7 @@ public class ContactFragment extends Fragment implements MyAsyncTask.OnGetContac
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_json, container, false);
+        View v = inflater.inflate(R.layout.fragment_contact, container, false);
         mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
