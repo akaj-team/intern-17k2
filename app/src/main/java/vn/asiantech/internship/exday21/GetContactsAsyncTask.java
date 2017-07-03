ccaf467505fc27f4095cf7d179a2233a96e47fe8
datTD123 <datbuitan96@gmail.com>
2017-07-03 20:59:26 +0700

    commit file

---
+++ app/src/main/java/vn/asiantech/internship/exday21/GetContactsAsyncTask.java
@@ -0,0 +1,78 @@
+package vn.asiantech.internship.exday21;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static com.nostra13.universalimageloader.core.ImageLoader.TAG;
+
+/**
+ * Copyright © 2016 AsianTech inc.
+ * Created by datbu on 03-07-2017.
+ */
+class GetContactsAsyncTask extends AsyncTask<JsonItem, Void, ArrayList<JsonItem>> {
+    private ArrayList<JsonItem> mJsonItems;
+    private OnCallBack mOnCallBack;
+
+    GetContactsAsyncTask(OnCallBack mOnCallBack) {
+        this.mJsonItems = new ArrayList<>();
+        this.mOnCallBack = mOnCallBack;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mOnCallBack.onCall();
+    }
+
+    @Override
+    protected ArrayList<JsonItem> doInBackground(JsonItem... arg0) {
+        HttpHandler httpHandler = new HttpHandler();
+        // Making a request to url and getting response
+        String url = "http://api.androidhive.info/contacts/";
+        String jsonStr = httpHandler.makeServiceCall(url);
+        Log.e(TAG, "Response from url: " + jsonStr);
+        if (jsonStr != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+                JSONArray contacts = jsonObj.getJSONArray("contacts");
+                // looping through All Contacts
+                for (int i = 0; i < contacts.length(); i++) {
+                    JSONObject c = contacts.getJSONObject(i);
+
+                    String name = c.getString("name");
+                    String email = c.getString("email");
+                    // Phone node is JSON Object
+                    JSONObject phone = c.getJSONObject("phone");
+                    String mobile = phone.getString("mobile");
+                    mJsonItems.add(new JsonItem(name, email, mobile));
+                    Log.d("taggg", "doInBackground: "+mJsonItems.size());
+                }
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<JsonItem> jsonItems) {
+        super.onPostExecute(jsonItems);
+        mOnCallBack.onCallBack(mJsonItems);
+    }
+
+    /**
+     * Copyright © 2016 AsianTech inc.
+     * Created by datbu on 03-07-2017.
+     */
+    interface OnCallBack {
+        void onCall();
+
+        void onCallBack(ArrayList<JsonItem> jsonItems);
+    }
+}
--- app/src/main/java/vn/asiantech/internship/exday21/JsonActivity.java
+++ app/src/main/java/vn/asiantech/internship/exday21/JsonActivity.java
@@ -1,118 +1,57 @@
 package vn.asiantech.internship.exday21;
 
 import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 import vn.asiantech.internship.R;
-import vn.asiantech.internship.exday7.MainActivity;
 
 /**
  * Copyright © 2016 AsianTech inc.
  * Created by datbu on 03-07-2017.
  */
 public class JsonActivity extends AppCompatActivity {
-    private String TAG = MainActivity.class.getSimpleName();
     private ProgressDialog mProgressDialog;
-    private ArrayList<JsonItem> mJsonItems;
     private RecyclerView mRecyclerView;
+    private GetContactsAsyncTask mGetContactsAsyncTask;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_json);
-        mJsonItems = new ArrayList<>();
         mRecyclerView = (RecyclerView) findViewById(R.id.rvJson);
-        new GetContacts().execute();
+        LinearLayoutManager manager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(manager);
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("Please wait...");
+        mProgressDialog.setCancelable(false);
+
+        mGetContactsAsyncTask = new GetContactsAsyncTask(new GetContactsAsyncTask.OnCallBack() {
+            @Override
+            public void onCall() {
+                mProgressDialog.show();
+            }
+            @Override
+            public void onCallBack(ArrayList<JsonItem> jsonItems) {
+                JsonAdapter adapter = new JsonAdapter(jsonItems);
+                mRecyclerView.setAdapter(adapter);
+                mProgressDialog.dismiss();
+            }
+        });
+        mGetContactsAsyncTask.execute();
     }
 
-    /**
-     * Async task class to get json by making HTTP call
-     */
-    private class GetContacts extends AsyncTask<JsonItem, Void, ArrayList<JsonItem>> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Showing progress dialog
-            mProgressDialog = new ProgressDialog(JsonActivity.this);
-            mProgressDialog.setMessage("Please wait...");
-            mProgressDialog.setCancelable(false);
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected ArrayList<JsonItem> doInBackground(JsonItem... arg0) {
-            HttpHandler sh = new HttpHandler();
-            // Making a request to url and getting response
-            String url = "http://api.androidhive.info/contacts/";
-            String jsonStr = sh.makeServiceCall(url);
-            Log.e(TAG, "Response from url: " + jsonStr);
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-                    JSONArray contacts = jsonObj.getJSONArray("contacts");
-                    // looping through All Contacts
-                    for (int i = 0; i < contacts.length(); i++) {
-                        JSONObject c = contacts.getJSONObject(i);
-
-                        String name = c.getString("name");
-                        String email = c.getString("email");
-                        // Phone node is JSON Object
-                        JSONObject phone = c.getJSONObject("phone");
-                        String mobile = phone.getString("mobile");
-                        mJsonItems.add(new JsonItem(name, email, mobile));
-                    }
-                } catch (final JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    });
-                }
-            } else {
-                Log.e(TAG, "Couldn't get json from server.");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(getApplicationContext(),
-                                "Couldn't get json from server. Check LogCat for possible errors!",
-                                Toast.LENGTH_LONG)
-                                .show();
-                    }
-                });
-
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<JsonItem> result) {
-            super.onPostExecute(result);
-            // Dismiss the progress dialog
-            if (mProgressDialog.isShowing())
-                mProgressDialog.dismiss();
-            LinearLayoutManager manager = new LinearLayoutManager(getApplicationContext());
-            mRecyclerView.setLayoutManager(manager);
-            JsonAdapter adapter = new JsonAdapter(mJsonItems);
-            mRecyclerView.setAdapter(adapter);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mGetContactsAsyncTask != null) {
+            mGetContactsAsyncTask.cancel(true);
         }
     }
 }
