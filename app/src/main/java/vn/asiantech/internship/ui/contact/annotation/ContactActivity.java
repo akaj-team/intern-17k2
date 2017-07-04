e0dece50064d8027fe832991403525edc1638590
hainq94@gmail.com <hainq94@gmail.com>
2017-07-04 11:25:04 +0700

    commit all

--- app/src/main/java/vn/asiantech/internship/ui/contact/annotation/ContactActivity.java
+++ app/src/main/java/vn/asiantech/internship/ui/contact/annotation/ContactActivity.java
@@ -2,9 +2,13 @@ package vn.asiantech.internship.ui.contact.annotation;
 
 import android.support.v7.app.AppCompatActivity;
 
+import org.androidannotations.annotations.EActivity;
+
+import vn.asiantech.internship.R;
+
 /**
  * Created by quanghai on 04/07/2017.
  */
-
+@EActivity(R.layout.activity_contact_annotation)
 public class ContactActivity extends AppCompatActivity {
 }
