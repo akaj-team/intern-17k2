a668c556b1f5c83a49d0d1b996f3805dc992085c
at-ducle <leanhduc2015@gmail.com>
2017-07-04 08:21:53 +0700

    add contruction

--- app/src/main/java/vn/asiantech/internship/apis/ContactApi.java
+++ app/src/main/java/vn/asiantech/internship/apis/ContactApi.java
@@ -4,6 +4,9 @@ package vn.asiantech.internship.apis;
  * Created by ducle on 03/07/2017.
  * ContactApi contain api
  */
-public class ContactApi {
+public final class ContactApi {
     public static final String URL = "http://api.androidhive.info/contacts/";
+
+    private ContactApi() {
+    }
 }
