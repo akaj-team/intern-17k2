cf5401b0ce7e6cf72565ea1c3f5e4f6a6366374a
hainq94@gmail.com <hainq94@gmail.com>
2017-07-04 08:24:03 +0700

    commit all

--- app/src/main/java/vn/asiantech/internship/handler/HttpHandler.java
+++ app/src/main/java/vn/asiantech/internship/handler/HttpHandler.java
@@ -1,11 +1,15 @@
 package vn.asiantech.internship.handler;
 
+import android.util.Log;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
@@ -14,21 +18,27 @@ import java.nio.charset.Charset;
  * Created by quanghai on 03/07/2017.
  */
 public class HttpHandler {
+    private static final String TAG = HttpHandler.class.getSimpleName();
+
     public HttpHandler() {
     }
 
     public String makeServiceCall(String reqUrl) {
-        String response;
+        String response = null;
         try {
             URL url = new URL(reqUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
-            // read the response
             InputStream in = new BufferedInputStream(conn.getInputStream());
             response = convertStreamToString(in);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
         } catch (Exception e) {
-            e.getMessage();
-            return  null;
+            Log.e(TAG, "Exception: " + e.getMessage());
         }
         return response;
     }
@@ -42,13 +52,12 @@ public class HttpHandler {
                 sb.append(line).append('\n');
             }
         } catch (IOException e) {
-            e.getMessage();
-            return null;
+            e.printStackTrace();
         } finally {
             try {
                 is.close();
             } catch (IOException e) {
-                e.getMessage();
+                e.printStackTrace();
             }
         }
         return sb.toString();
