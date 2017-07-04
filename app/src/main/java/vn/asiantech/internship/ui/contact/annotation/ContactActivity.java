27bf9f7e9e4a4dad8de3ce13e1c5e558f7887e90
hainq94@gmail.com <hainq94@gmail.com>
2017-07-04 11:17:07 +0700

    config gradle

---
+++ app/src/main/java/vn/asiantech/internship/ui/contact/annotation/ContactActivity.java
@@ -0,0 +1,10 @@
+package vn.asiantech.internship.ui.contact.annotation;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by quanghai on 04/07/2017.
+ */
+
+public class ContactActivity extends AppCompatActivity {
+}
