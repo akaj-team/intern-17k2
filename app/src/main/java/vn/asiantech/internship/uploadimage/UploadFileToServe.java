070c5b94ac39d64a2dae762916032715af8a6210
DieuHang091095 <dieuhang091095@gmail.com>
2017-07-06 16:10:23 +0700

    load image

---
+++ app/src/main/java/vn/asiantech/internship/uploadimage/UploadFileToServe.java
@@ -0,0 +1,78 @@
+package vn.asiantech.internship.uploadimage;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Author AsianTech Inc.
+ * Created by at-hangtran on 05/07/2017.
+ */
+class UploadFileToServe extends AsyncTask<String, Integer, String> {
+    private static final String URL = "http://2.pik.vn/";
+    private static final String TAG = "error";
+    private MediaType mMediaType = MediaType.parse("application/x-www-form-urlencoded");
+    private OnAsyncResponseListener mListener = null;
+
+    UploadFileToServe(OnAsyncResponseListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String link = "";
+        RequestBody body = RequestBody.create(mMediaType, strings[0]);
+        Request request = new Request.Builder()
+                .url(URL)
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .build();
+        OkHttpClient client = new OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response != null) {
+                String Json = response.body().string();
+                if (Json != null) {
+                    JSONObject jsonObj = new JSONObject(Json);
+                    String image = jsonObj.getString("saved");
+                    link = URL + image;
+                }
+            }
+        } catch (JSONException | IOException e) {
+            Log.d(TAG, "doInBackground: " + e.toString());
+        }
+        return link;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        mListener.onProcessFinish(result);
+    }
+
+    /*
+     * Used to get contacts from AsyncTask to MainActivity
+     */
+    interface OnAsyncResponseListener {
+        void onProcessFinish(String link);
+    }
+}
---
+++ app/src/main/java/vn/asiantech/internship/uploadimage/UploadImageActivity.java
@@ -0,0 +1,120 @@
+package vn.asiantech.internship.uploadimage;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import vn.asiantech.internship.R;
+
+/**
+ * Author AsianTech Inc.
+ * Created by at-hangtran on 05/07/2017.
+ */
+public class UploadImageActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1;
+    private static final String TAG = "error";
+    private ImageView mImgShowImage;
+    private ProgressDialog mProgressDialog;
+    private UploadFileToServe mUploadFileToServe = new UploadFileToServe(new UploadFileToServe.OnAsyncResponseListener() {
+        @Override
+        public void onProcessFinish(String link) {
+            if (link != null) {
+                ImageLoader.getInstance().displayImage(link, mImgShowImage);
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                }
+            }
+        }
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_image);
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
+        mImgShowImage = (ImageView) findViewById(R.id.imgShowImage);
+        Button btnUpload = (Button) findViewById(R.id.btnUpload);
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    private String getRealPathFromUri(Uri tempUri) {
+        Cursor cursor;
+        try {
+            String[] images = {MediaStore.Images.Media.DATA};
+            cursor = getContentResolver().query(tempUri, images, null, null, null);
+            int column_index;
+            String path;
+            if (cursor != null) {
+                column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                path = cursor.getString(column_index);
+                cursor.close();
+            } else {
+                path = null;
+            }
+            return path;
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException" + e.toString());
+        }
+        return "";
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            if (requestCode == REQUEST_CODE) {
+                Uri uriImage = data.getData();
+                mUploadFileToServe.execute(getFileToByte(getRealPathFromUri(uriImage)));
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage("Please wait...");
+                mProgressDialog.setCancelable(false);
+                mProgressDialog.show();
+            }
+        }
+    }
+
+    public String getFileToByte(String path) {
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        String encodeString = Base64.encodeToString(bytes, Base64.NO_WRAP);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("image=");
+        try {
+            stringBuilder.append(URLEncoder.encode("data:image/jpg;base64," + encodeString, "utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        stringBuilder.append("==");
+        return stringBuilder.toString();
+    }
+}
