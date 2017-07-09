19a7e6223991080332966eaae537517b15b466e6
hainq94@gmail.com <hainq94@gmail.com>
2017-07-09 20:46:14 +0700

    commit all

--- app/src/main/java/vn/asiantech/internship/ui/chart/CustomView.java
+++ app/src/main/java/vn/asiantech/internship/ui/chart/CustomView.java
@@ -4,19 +4,26 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 /**
+ *
  * Created by quanghai on 06/07/2017.
  */
-
 public class CustomView extends View {
     private static final int MARGIN = 30;
     private Paint mPaint;
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.0f;
+
     public CustomView(Context context) {
         super(context);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
     public CustomView(Context context, @Nullable AttributeSet attrs) {
@@ -35,26 +42,61 @@ public class CustomView extends View {
             mPaint.setColor(Color.BLACK);
             mPaint.setStrokeWidth(5);
         }
+        canvas.scale(mScaleFactor, mScaleFactor);
+        drawPivot(canvas);
+        drawParabol(canvas);
+    }
 
+    //y = 3x^2 - 4x + 1
+    private void drawParabol(Canvas canvas) {
+        int unit = 100;
+        float x = getWidth() / 2 + 10 * unit;
+        float y = getHeight() / 2 - (3 * x * x - 4 * x + 1) * unit;
+        for (double i = -10; i < 10; i += 0.05f) {
+            float x2 = (float) (getWidth() / 2 + i * unit);
+            float y2 = (float) (getHeight() / 2 - (3 * i * i - 4 * i + 1) * unit);
+            canvas.drawLine(x, y, x2, y2, mPaint);
+            x = x2;
+            y = y2;
+        }
+    }
+
+    private void drawPivot(Canvas canvas) {
         canvas.drawLine(MARGIN, getHeight() / 2, getWidth() - MARGIN, getHeight() / 2, mPaint);
         canvas.drawLine(getWidth() / 2, MARGIN, getWidth() / 2, getHeight() - MARGIN, mPaint);
 
-        //y = 3x^2 - 4x + 1
-//        float x1 = getWidth() / 2; float y1 = getHeight() / 2 - 1;
-//        float x2 = x1 + 1; float y2 = getHeight() / 2;
-//        float x3 = x1 + 1/2; float y3 = getHeight() / 2;
-//        canvas.drawPoint(x1, y1, mPaint);
-//        canvas.drawPoint(x2, y2, mPaint);
-//        canvas.drawPoint(x3, y3, mPaint);
-        float xA = 5gi;
-        float yA = (3 * xA * xA) - (4 * xA) + 1;
-        for(float xB = xA; xB < getWidth();){
-            xB = xB + 0.1f;
-            float yB = (3 * xB * xB) - (4 * xB) + 1;
-            canvas.drawLine(xA, yA, xB, yB, mPaint);
-//            canvas.drawPoint(xB, yB, mPaint);
-            xA = xB;
-            yA = yB;
+        Path path = new Path();
+        path.moveTo(getWidth() - MARGIN, getHeight() / 2 - 10);
+        path.lineTo(getWidth(), getHeight() / 2);
+        path.lineTo(getWidth() - MARGIN, getHeight() / 2 + 10);
+
+        path.moveTo(getWidth() / 2 + 10, MARGIN);
+        path.lineTo(getWidth() / 2, 0);
+        path.lineTo(getWidth() / 2 - 10, MARGIN);
+        canvas.drawPath(path, mPaint);
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleDetector.onTouchEvent(event);
+        return true;
+    }
+
+    /**
+     * Created by quanghai on 06/07/2017.
+     */
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            invalidate();
+            return true;
         }
     }
 }
