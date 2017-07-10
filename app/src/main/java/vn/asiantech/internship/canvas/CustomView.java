77877563e212448437e5de80d7f44837ff92f7b8
DieuHang091095 <dieuhang091095@gmail.com>
2017-07-10 10:43:36 +0700

    fix warning

--- app/src/main/java/vn/asiantech/internship/canvas/CustomView.java
+++ app/src/main/java/vn/asiantech/internship/canvas/CustomView.java
@@ -8,7 +8,6 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
