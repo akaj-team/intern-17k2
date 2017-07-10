f546ed20ca2961ba4c608e91b9aa51615259884d
Dinh Vo X <ignore8542@gmail.com>
2017-07-10 16:47:15 +0700

    create branch

---
+++ app/src/main/java/vn/asiantech/internship/day26/TestCaseActivity.java
@@ -0,0 +1,18 @@
+package vn.asiantech.internship.day26;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.asiantech.internship.R;
+
+/**
+ * TestCaseActivity
+ */
+public class TestCaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_case);
+    }
+}
