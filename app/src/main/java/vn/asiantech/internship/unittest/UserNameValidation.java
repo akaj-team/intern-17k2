1f4e78bae8a95d8a92bd4ca4d174e44a7078f8e6
DieuHang091095 <dieuhang091095@gmail.com>
2017-07-11 23:35:35 +0700

    fix comments

--- app/src/main/java/vn/asiantech/internship/unittest/UserNameValidation.java
+++ app/src/main/java/vn/asiantech/internship/unittest/UserNameValidation.java
@@ -10,8 +10,8 @@ public final class UserNameValidation {
     private UserNameValidation() {
     }
 
-    public static boolean checkUserNameLength(String username) {
-        return username.length() >= 6 && username.length() <= 24;
+    public static boolean checkUserNameLength(String userName) {
+        return userName.length() >= 6 && userName.length() <= 24;
     }
 
     public static boolean checkUserNameSpace(String useName) {
