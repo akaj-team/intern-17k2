8bbe833f4850cbfe4c5244f0a37fc2baa233eacc
hainq94@gmail.com <hainq94@gmail.com>
2017-07-17 11:25:54 +0700

    commit all

--- app/src/androidTest/java/vn/asiantech/internship/NoteTest.java
+++ app/src/androidTest/java/vn/asiantech/internship/NoteTest.java
@@ -12,6 +12,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -31,50 +32,50 @@ public class NoteTest {
 
     @Test
     public void checkNewNote() {
-        onView(withId(R.id.imgNewNote)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.edtNoteTitle)).check(matches(isDisplayed())).perform(typeText("New note"), closeSoftKeyboard());
-        onView(withId(R.id.edtInputContent)).check(matches(isDisplayed())).perform(typeText("Content"), closeSoftKeyboard());
-        onView(withId(R.id.imgSaveNote)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.recyclerViewNote)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.imgNewNote)).perform(click()).check(doesNotExist());
+        onView(withId(R.id.edtNoteTitle)).perform(typeText("New note"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.edtInputContent)).perform(typeText("Content"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.imgSaveNote)).perform(click()).check(doesNotExist());
+        onView(withId(R.id.recyclerViewNote)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click())).check(doesNotExist());
         onView(withId(R.id.edtEditNoteTitle)).check(matches(withText("New note")));
         onView(withId(R.id.edtEditInputContent)).check(matches(withText("Content")));
     }
 
     @Test
     public void checkNewNoteFail() {
-        onView(withId(R.id.imgNewNote)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.edtInputContent)).check(matches(isDisplayed())).perform(typeText("Content"), closeSoftKeyboard());
-        onView(withId(R.id.imgSaveNote)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.imgNewNote)).perform(click()).check(doesNotExist());
+        onView(withId(R.id.edtInputContent)).perform(typeText("Content"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.imgSaveNote)).perform(click()).check(matches(isDisplayed()));
     }
 
     @Test
     public void checkSelectNote() {
-        onView(withId(R.id.recyclerViewNote)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.recyclerViewNote)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click())).check(doesNotExist());
     }
 
     @Test
     public void checkEditNoteCompleted() {
-        onView(withId(R.id.recyclerViewNote)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        onView(withId(R.id.edtEditNoteTitle)).check(matches(isDisplayed())).perform(clearText(), typeText("Edit title"), closeSoftKeyboard());
-        onView(withId(R.id.imgEditNote)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.recyclerViewNote)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.recyclerViewNote)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click())).check(doesNotExist());
+        onView(withId(R.id.edtEditNoteTitle)).perform(clearText(), typeText("Edit title"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.imgEditNote)).perform(click()).check(doesNotExist());
+        onView(withId(R.id.recyclerViewNote)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click())).check(doesNotExist());
         onView(withId(R.id.edtEditNoteTitle)).check(matches(withText("Edit title")));
     }
 
     @Test
     public void checkEditNoteFail() {
-        onView(withId(R.id.recyclerViewNote)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        onView(withId(R.id.edtEditNoteTitle)).check(matches(isDisplayed())).perform(clearText(), closeSoftKeyboard());
-        onView(withId(R.id.imgEditNote)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.recyclerViewNote)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click())).check(doesNotExist());
+        onView(withId(R.id.edtEditNoteTitle)).perform(clearText(), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.imgEditNote)).perform(click()).check(matches(isDisplayed()));
     }
 
     @Test
     public void checkDeleteNote() {
-        onView(withId(R.id.imgNewNote)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.edtNoteTitle)).check(matches(isDisplayed())).perform(typeText("New note"), closeSoftKeyboard());
-        onView(withId(R.id.edtInputContent)).check(matches(isDisplayed())).perform(typeText("Content"), closeSoftKeyboard());
-        onView(withId(R.id.imgSaveNote)).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.recyclerViewNote)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-        onView(withId(R.id.imgDeleteNote)).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.imgNewNote)).perform(click()).check(doesNotExist());
+        onView(withId(R.id.edtNoteTitle)).perform(typeText("New note"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.edtInputContent)).perform(typeText("Content"), closeSoftKeyboard()).check(matches(isDisplayed()));
+        onView(withId(R.id.imgSaveNote)).perform(click()).check(doesNotExist());
+        onView(withId(R.id.recyclerViewNote)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click())).check(doesNotExist());
+        onView(withId(R.id.imgDeleteNote)).perform(click()).check(doesNotExist());
     }
 }
