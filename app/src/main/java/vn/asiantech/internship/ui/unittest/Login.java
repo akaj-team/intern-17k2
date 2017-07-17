70695aec9cc2adf3d20be5516d95d5e242772d74
hainq94@gmail.com <hainq94@gmail.com>
2017-07-17 09:01:19 +0700

    fi ci

--- app/src/main/java/vn/asiantech/internship/ui/unittest/Login.java
+++ app/src/main/java/vn/asiantech/internship/ui/unittest/Login.java
@@ -4,7 +4,7 @@ package vn.asiantech.internship.ui.unittest;
  *
  * Created by quanghai on 11/07/2017.
  */
-public final class Login {
+public class Login {
     public static boolean getMinLengthUserName(String userName) {
         return userName.length() >= 6;
     }
