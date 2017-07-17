43643e515f4d599a2e52e2b074743d36c64da145
hainq94@gmail.com <hainq94@gmail.com>
2017-07-17 09:23:53 +0700

    fix comment ci

--- app/src/main/java/vn/asiantech/internship/ui/unittest/Login.java
+++ app/src/main/java/vn/asiantech/internship/ui/unittest/Login.java
@@ -1,11 +1,10 @@
 package vn.asiantech.internship.ui.unittest;
 
 /**
- *
  * Created by quanghai on 11/07/2017.
  */
 public class Login {
-    public Login() {
+    Login() {
     }
 
     public static boolean getMinLengthUserName(String userName) {
