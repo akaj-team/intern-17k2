862a6340e7567e09bc65251ad7567d0e64537b48
at-ducle <leanhduc2015@gmail.com>
2017-07-18 14:34:43 +0700

    zoom

--- app/src/main/java/vn/asiantech/internship/activity/ParabolaActivity.java
+++ app/src/main/java/vn/asiantech/internship/activity/ParabolaActivity.java
@@ -3,9 +3,6 @@ package vn.asiantech.internship.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
 
 import vn.asiantech.internship.ui.draw.DrawParabolaView;
 
@@ -13,35 +10,12 @@ import vn.asiantech.internship.ui.draw.DrawParabolaView;
  * Created by ducle on 11/07/2017.
  * ParabolaActivity contain custom view
  */
-public class ParabolaActivity extends AppCompatActivity implements View.OnTouchListener {
-    private static final String TAG = ParabolaActivity.class.getSimpleName();
-    private DrawParabolaView mDrawParabolaView;
-    private float mHoldX;
-    private float mHoldY;
+public class ParabolaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mDrawParabolaView = new DrawParabolaView(this);
-        mDrawParabolaView.setOnTouchListener(this);
+        DrawParabolaView mDrawParabolaView = new DrawParabolaView(this);
         setContentView(mDrawParabolaView);
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mHoldX = event.getX();
-                mHoldY = event.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                mDrawParabolaView.move(event.getX() - mHoldX, event.getY() - mHoldY);
-                mHoldX = event.getX();
-                mHoldY = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                break;
-        }
-        return true;
     }
 }
--- app/src/main/java/vn/asiantech/internship/ui/draw/DrawParabolaView.java
+++ app/src/main/java/vn/asiantech/internship/ui/draw/DrawParabolaView.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -15,11 +16,22 @@ import android.view.View;
  * DrawParabolaView is view draw parabola
  */
 public class DrawParabolaView extends View {
+    private static final int INVALID_POINTER_ID = -1;
+    private int mFirstPointerId = INVALID_POINTER_ID;
     private static final String TAG = DrawParabolaView.class.getSimpleName();
     private Paint mPaint;
     private Path mPath;
     private float mOriginX = 0;
     private float mOriginY = 0;
+    private float mFirstHoldX;
+    private float mFirstHoldY;
+    private float mSecondHoldX;
+    private float mSecondHoldY;
+    private float mPivotPointX = 0;
+    private float mPivotPointY = 0;
+    private float mDistance;
+    private boolean mKeep = true;
+    private float k = 100;
 
     public DrawParabolaView(Context context) {
         super(context);
@@ -44,8 +56,7 @@ public class DrawParabolaView extends View {
         super.onDraw(canvas);
         setBackgroundColor(Color.BLACK);
         canvas.translate(mOriginX, mOriginY);
-        Log.d(TAG, "onDraw: " + mOriginX + " " + mOriginY);
-        float k = getWidth() / 2 / 5;
+
         //draw 2 lines
         canvas.drawLine(getWidth() / 2 - 100 * k, getHeight() / 2, getWidth() / 2 + 100 * k, getHeight() / 2, mPaint);
         canvas.drawLine(getWidth() / 2, getHeight() / 2 - 100 * k, getWidth() / 2, getHeight() / 2 + 100 * k, mPaint);
@@ -83,7 +94,6 @@ public class DrawParabolaView extends View {
             x = (float) i;
             y = getY((float) i);
         }
-
     }
 
     private float getXCanvas(float x, float k) {
@@ -102,5 +112,74 @@ public class DrawParabolaView extends View {
         mOriginX += oX;
         mOriginY += oY;
         invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                mFirstPointerId = event.getPointerId(0);
+                mFirstHoldX = event.getX();
+                mFirstHoldY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final int pointerFirst = event.findPointerIndex(mFirstPointerId);
+                if (event.getPointerCount() == 1) {
+                    move(event.getX(pointerFirst) - mFirstHoldX, event.getY(pointerFirst) - mFirstHoldY);
+                    mFirstHoldX = event.getX(pointerFirst);
+                    mFirstHoldY = event.getY(pointerFirst);
+                } else if (event.getPointerCount() == 2) {
+                    if (mKeep) {
+                        mSecondHoldX = event.getX(event.getPointerId(1));
+                        mSecondHoldY = event.getY(event.getPointerId(1));
+                        mDistance = getDistance(mFirstHoldX, mFirstHoldY, mSecondHoldX, mSecondHoldY);
+                        mKeep = false;
+                    } else {
+                        final int pointerSecond = event.findPointerIndex(event.getPointerId(1));
+                        float newDistance = getDistance(event.getX(pointerFirst), event.getY(pointerFirst), event.getX(pointerSecond
+                        ), event.getY(pointerSecond));
+                        if (newDistance > mDistance) {
+                            k += 10;
+                            invalidate();
+                            Log.d(TAG, "zoom out: ");
+                            mDistance = newDistance;
+                        } else {
+                            if (k > 10) {
+                                k -= 10;
+                                invalidate();
+                                Log.d(TAG, "zoom in: ");
+                                mDistance = newDistance;
+                            }
+                        }
+                        invalidate();
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mFirstPointerId = INVALID_POINTER_ID;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                mFirstPointerId = INVALID_POINTER_ID;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                // Extract the index of the pointer that left the touch sensor
+                final int pointerIndex0 = (event.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                        >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = event.getPointerId(pointerIndex0);
+                if (pointerId == mFirstPointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex0 == 0 ? 1 : 0;
+                    mFirstHoldX = event.getX(newPointerIndex);
+                    mFirstHoldY = event.getY(newPointerIndex);
+                    mFirstPointerId = event.getPointerId(newPointerIndex);
+                }
+                break;
+        }
+        return true;
+    }
+
+    private float getDistance(float x1, float y1, float x2, float y2) {
+        return (float) Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
     }
 }
