c21f3e8bca42fc9bbd6b101c9e7ee99a29e4be66
hainq94@gmail.com <hainq94@gmail.com>
2017-07-18 10:54:34 +0700

    add drag canvas

--- app/src/main/java/vn/asiantech/internship/ui/chart/CustomView.java
+++ app/src/main/java/vn/asiantech/internship/ui/chart/CustomView.java
@@ -7,12 +7,13 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
 /**
- *
  * Created by quanghai on 06/07/2017.
  */
 public class CustomView extends View {
@@ -22,6 +23,11 @@ public class CustomView extends View {
     private Paint mPaint;
     private ScaleGestureDetector mScaleDetector;
     private float mScaleFactor = 1.0f;
+    private float mLastTouchX;
+    private float mLastTouchY;
+    private float mPosX = getWidth() / 2;
+    private float mPosY = getHeight() / 2;
+    private float mDistance = 0;
 
     public CustomView(Context context) {
         super(context);
@@ -50,6 +56,7 @@ public class CustomView extends View {
         drawPivotY(canvas);
         drawArrow(canvas);
         drawParabol(canvas);
+        drawCircle(canvas);
     }
 
     //y = 3x^2 - 4x + 1
@@ -57,8 +64,8 @@ public class CustomView extends View {
         float x = getWidth() / 2 + 10 * mUnit;
         float y = getHeight() / 2 - (3 * x * x - 4 * x + 1) * mUnit;
         for (double i = -10; i < 10; i += 0.05f) {
-            float x2 = (float) (getWidth() / 2 + i * mUnit);
-            float y2 = (float) (getHeight() / 2 - (3 * i * i - 4 * i + 1) * mUnit);
+            float x2 = (float) (getWidth() / 2 + i * mUnit) + mPosX;
+            float y2 = (float) (getHeight() / 2 - (3 * i * i - 4 * i + 1) * mUnit) + mPosY;
             canvas.drawLine(x, y, x2, y2, mPaint);
             x = x2;
             y = y2;
@@ -67,36 +74,42 @@ public class CustomView extends View {
 
     private void drawArrow(Canvas canvas) {
         //draw 0
-        canvas.drawText("0", getWidth() / 2 - 50, getHeight() / 2 + 50, mPaint);
+        canvas.drawText("0", getWidth() / 2 - 50 + mPosX, getHeight() / 2 + 50 + mPosY, mPaint);
 
         Path path = new Path();
-        path.moveTo(getWidth() - MARGIN, getHeight() / 2 - 10);
-        path.lineTo(getWidth(), getHeight() / 2);
-        path.lineTo(getWidth() - MARGIN, getHeight() / 2 + 10);
+        path.moveTo(getWidth() - MARGIN + mDistance, getHeight() / 2 - 10 + mPosY);
+        path.lineTo(getWidth() + mDistance, getHeight() / 2 + mPosY);
+        path.lineTo(getWidth() - MARGIN + mDistance, getHeight() / 2 + 10 + mPosY);
 
-        path.moveTo(getWidth() / 2 + 10, MARGIN);
-        path.lineTo(getWidth() / 2, 0);
-        path.lineTo(getWidth() / 2 - 10, MARGIN);
+        path.moveTo(getWidth() / 2 + 10 + mPosX, MARGIN + mDistance);
+        path.lineTo(getWidth() / 2 + mPosX, 0 + mDistance);
+        path.lineTo(getWidth() / 2 - 10 + mPosX, MARGIN + mDistance);
         canvas.drawPath(path, mPaint);
     }
 
+    private void drawCircle(Canvas canvas) {
+        canvas.drawCircle(mPosX, mPosY, 100, mPaint);
+    }
+
     private void drawPivotX(Canvas canvas) {
-        canvas.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2, mPaint);
-        for (int i = getWidth() / 2; i < getWidth(); i += mUnit) {
-            canvas.drawLine(i, getHeight() / 2 + 10, i, getHeight() / 2 - 10, mPaint);
+        Log.d("Xxx", "drawPivotX: " + mDistance);
+        // canvas.drawLine(0 + mPosX, getHeight() / 2 + mPosY, getWidth() mPosX, getHeight() / 2 + mPosY, mPaint);
+        canvas.drawLine(0 + mDistance, getHeight() / 2 + mPosY, getWidth() + mDistance, getHeight() / 2 + mPosY, mPaint);
+        for (float i = getWidth() / 2; i < getWidth() - mPosX; i += mUnit) {
+            canvas.drawLine(i + mPosX, getHeight() / 2 + 10 + mPosY, i + mPosX, getHeight() / 2 - 10 + mPosY, mPaint);
         }
-        for (int i = getWidth() / 2; i > 0; i -= mUnit) {
-            canvas.drawLine(i, getHeight() / 2 + 10, i, getHeight() / 2 - 10, mPaint);
+        for (float i = getWidth() / 2; i > 0 - mPosX; i -= mUnit) {
+            canvas.drawLine(i + mPosX, getHeight() / 2 + 10 + mPosY, i + mPosX, getHeight() / 2 - 10 + mPosY, mPaint);
         }
     }
 
     private void drawPivotY(Canvas canvas) {
-        canvas.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight(), mPaint);
-        for (int i = getHeight() / 2; i < getHeight(); i += mUnit) {
-            canvas.drawLine(getWidth() / 2 - 10, i, getWidth() / 2 + 10, i, mPaint);
+        canvas.drawLine(getWidth() / 2 + mPosX, 0 + mDistance, getWidth() / 2 + mPosX, getHeight() + mDistance, mPaint);
+        for (float i = getHeight() / 2; i < getHeight() - mPosY; i += mUnit) {
+            canvas.drawLine(getWidth() / 2 - 10 + mPosX, i + mPosY, getWidth() / 2 + 10 + mPosX, i + mPosY, mPaint);
         }
-        for (int i = getHeight() / 2; i > 0; i -= mUnit) {
-            canvas.drawLine(getWidth() / 2 - 10, i, getWidth() / 2 + 10, i, mPaint);
+        for (float i = getHeight() / 2; i > 0 - mPosY; i -= mUnit) {
+            canvas.drawLine(getWidth() / 2 - 10 + mPosX, i + mPosY, getWidth() / 2 + 10 + mPosX, i + mPosY, mPaint);
         }
     }
 
@@ -106,10 +119,36 @@ public class CustomView extends View {
         return true;
     }
 
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        performClick();
+//        mScaleDetector.onTouchEvent(event);
+//        return true;
+//    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        performClick();
-        mScaleDetector.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastTouchX = event.getX();
+                mLastTouchY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float x = event.getX();
+                float y = event.getY();
+
+                float dx = x - mLastTouchX;
+                float dy = y - mLastTouchY;
+                mDistance = dx;
+
+                mPosX += dx;
+                mPosY += dy;
+                invalidate();
+
+                mLastTouchX = x;
+                mLastTouchY = y;
+                break;
+        }
         return true;
     }
 
