6b5463fb578c5fc995a3dde6eb27ea1f8a994c36
anhhuy173 <phamnguyenanhhuy@gmail.com>
2017-07-27 10:25:18 +0700

    start activity from splash

--- app/src/main/java/vn/asiantech/internship/ui/splash/SplashActivity.java
+++ app/src/main/java/vn/asiantech/internship/ui/splash/SplashActivity.java
@@ -9,25 +9,28 @@ import android.widget.Button;
 
 import vn.asiantech.internship.R;
 import vn.asiantech.internship.ui.main.MainActivity;
+import vn.asiantech.internship.ui.mockito.LoginActivity;
 
 /**
  * Activity for Splash
  * Created by huypham on 15/06/2017.
  */
 public class SplashActivity extends AppCompatActivity {
+
     private Button mBtnEx6;
+    private Button mBtnMockito;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         initSplashView();
         onClickButton();
     }
 
     private void initSplashView() {
         mBtnEx6 = (Button) findViewById(R.id.btnEx6);
+        mBtnMockito = (Button) findViewById(R.id.btnMockito);
     }
 
     private void onClickButton() {
@@ -36,6 +39,13 @@ public class SplashActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intentEx6 = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intentEx6);
+            }
+        });
+        mBtnMockito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMockito = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intentMockito);
             }
         });
     }
